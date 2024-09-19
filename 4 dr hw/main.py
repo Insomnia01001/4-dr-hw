960a21e16851fe2114783f9149f125a0d13ebc70
Insomnia01001 <167763964+Insomnia01001@users.noreply.github.com>
2024-09-19 21:49:36 +0300

    Add files via upload

---
+++ 4 dr hw/main.py
@@ -0,0 +1,22 @@
+text= input("harf kiriting: ")
+print(f"{text}{text}{text}{text}{text}")
+text2= int(input("son kiriting: "))
+text3= int(input("son kiriting: "))
+javob1=text2+text3
+print(javob1)
+javob2=text2-text3
+print(javob2)
+javob3=text2*text3
+print(javob3)
+javob4=text2/text3
+print(javob4)
+son1 = int(input("son kiriting: "))
+son_=son1**3
+print(son_)
+
+# son2= input("son kiriting: ")
+# pow_son= pow(son2)
+# abs_son= abs(son2)
+# print(pow_son)
+# print(abs_son)
+
